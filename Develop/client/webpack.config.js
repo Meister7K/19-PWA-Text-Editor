b8abd882ba657a7529c1ce620b5b8875fdf3284e
7K <kmanfinkel17@gmail.com>
2023-05-31 20:29:35 -0500

    edits

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -41,14 +41,13 @@ module.exports = () => {
         swSrc: './src/src-sw.js',
         swDest: 'sw.js',
       }),
-      new MiniCssExtractPlugin(),
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: ['style.loader', 'css-loader'],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
