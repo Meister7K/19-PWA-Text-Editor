4ba8feba287e6b7606f4c62edcf270de0707bbb9
7K <kmanfinkel17@gmail.com>
2023-05-31 20:15:48 -0500

    cleaned up install and DB

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,32 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const pwaDb = await openDB('pwa', 1);
+
+  const tx = pwaDb.transaction('pwa','readwrite');
+
+  const store = tx.objectStore('pwa');
+
+  const request = store.put({text: content});
+
+  const result = await request;
+  console.log('data Saved', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  const pwaDb = await openDB('pwa',1);
+
+  const tx = pwaDb.transaction('pwa',1);
+
+  const store = tx.objectStore('pwa');
+
+  const request = store.getAll();
+
+  const result = await request;
+  console.log('result.value', result);
+  return result.value;
+};
 
 initdb();
