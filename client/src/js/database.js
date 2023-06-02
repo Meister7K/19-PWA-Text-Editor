bf53e38d0e17adb4e968991e878d312adefc632f
7K <kmanfinkel17@gmail.com>
2023-06-01 20:12:42 -0500

    file structure setup

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('pwa', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('pwa')) {
+        console.log('pwa database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('pwa', { keyPath: 'id', autoIncrement: true });
+      console.log('pwa database created');
     },
   });
 
